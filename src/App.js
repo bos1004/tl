6aefd9d2618afb72a0108d21cf80bdb01d25d674
bos1009 <bos1009@naver.com>
2024-01-24 09:24:27 +0900

    map.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import logo from './logo.svg';
 import Customer from './components/Customer';
 
-const customer = {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/any',
-  'name' : '홍길동',
-  'birthday' : '760225',
-  'gender' : '남자',
-  'job' : '직장인',
-
-}
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : '홍길동',
+    'birthday' : '760225',
+    'gender' : '남자',
+    'job' : '직장인',
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '이재명',
+    'birthday' : '630331',
+    'gender' : '남자',
+    'job' : '국회의원',
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '윤석열',
+    'birthday' : '660205',
+    'gender' : '남자',
+    'job' : '대통령',
+  }
+]
 
 
 function App() {
   return (
-    <Customer 
-      id={customer.id}
-      image={customer.image}
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}      
-    />
+    <div> 
+      {
+        customers.map(c => {
+          return(
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+          }
+        )
+      }    
+    </div>
   );
 }
 
